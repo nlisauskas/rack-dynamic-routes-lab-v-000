26109d78618c086a42022c7062293ea1746e656c
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-14 09:36:22 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,31 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+      item = req.path.split(/items/).last
+
+      if @@items.include?(item)
+
+      resp.write "#{item.price}"
+
+      end
+
+    elsif req.path.match(/items/)
+
+      resp.write "Item not found"
+      resp.status = 400
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+
+end
